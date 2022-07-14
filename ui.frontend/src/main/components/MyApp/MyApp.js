812b0a30254efa92ea52b4310547ab462b37aa9a
Rafael Reis <rafaeloliveira63@yahoo.com>
2022-07-14 10:22:18 -0300

    Feat: adding the component datePicker

--- ui.frontend/src/main/components/MyApp/MyApp.js
+++ ui.frontend/src/main/components/MyApp/MyApp.js
@@ -6,6 +6,7 @@ import ComponenteCheckbox from './ComponenteCheckbox';
 import ComponenteLink from './ComponenteLink';
 import ComponenteSelect from './ComponenteSelect';
 import ComponenteRadio from './ComponenteRadio';
+import ComponenteDate from './ComponenteDate';
 
 class MyApp extends ReactHabitat.Bootstrapper {
     constructor() {
@@ -22,6 +23,7 @@ class MyApp extends ReactHabitat.Bootstrapper {
         builder.register(ComponenteLink).as('ComponenteLink');
         builder.register(ComponenteSelect).as('ComponenteSelect');
         builder.register(ComponenteRadio).as('ComponenteRadio');
+        builder.register(ComponenteDate).as('ComponenteDate');
 
         // Finally, set the container:
         this.setContainer(builder.build());
