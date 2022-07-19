c02f5bb7b3b9402a4a6b76f69244e0a396dcd8e5
Eduardo Borges <eduardo.borges@compasso.com.br>
2022-07-19 10:03:33 -0300

    carousel with image and texts examples has been added on ui.apps

--- ui.apps/src/main/content/jcr_root/apps/venia/clientlibs/clientlib-base/js/carousel.js
+++ ui.apps/src/main/content/jcr_root/apps/venia/clientlibs/clientlib-base/js/carousel.js
@@ -1,19 +1,17 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:20,
-  nav:true,
-  stagePadding: 10,
-  responsive:{
-      0:{
-          items:1
+$(".owl-carousel").owlCarousel({
+  loop: false,
+  margin: 20,
+  nav: true,
+  stagePadding: 0,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+        items: 3,
       },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
-})
-
-console.log("TESTE")
+  },
+});
